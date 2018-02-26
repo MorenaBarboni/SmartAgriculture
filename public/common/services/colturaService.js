6ba02556a1a8813ee0849a9bef9d084fe077ed74
Morena Barboni <morena.barboni@studenti.unicam.it>
2018-02-26 12:01:27 +0100

    coltura service

---
+++ public/common/services/colturaService.js
@@ -0,0 +1,33 @@
+(function () {
+  angular.module("greenhouseApp").service("colturaService", colturaService);
+
+  colturaService.$inject = ["$http", "authentication"];
+  function colturaService($http, authentication) {
+
+    //Ottiene una coltura per nome
+    getColturaByName = function (nomeColtura) {
+      return $http
+        .get("/api/coltura/" + nomeColtura, {
+          headers: {
+            Authorization: "Bearer " + authentication.getToken()
+          }
+        })
+        .then(handleSuccess, handleError);
+    };
+
+
+    //funzioni private
+    function handleSuccess(res) {
+      return res.data;
+    }
+
+    function handleError(res) {
+      return $q.reject(res.data);
+    }
+
+    return {
+      getColturaByName: getColturaByName
+
+    };
+  }
+})();

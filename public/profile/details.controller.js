a92767c8eb7723608c9fd4c90335dec9bc99c78a
Morena Barboni <morena.barboni@studenti.unicam.it>
2018-02-28 13:21:00 +0100

    setStato

--- public/profile/details.controller.js
+++ public/profile/details.controller.js
@@ -14,6 +14,12 @@
     vm.numSensore = $routeParams.numSensore; //Numero di sensore
 
     vm.colturaCorrente = {}
+    vm.colturaDefault = {};//Dati di default della coltura corrente
+
+    //Parametri modifica coltura
+    vm.modificaColtStato;
+    vm.modificaColtMin;
+    vm.modificaColtMax;
 
     initController();
 
@@ -26,6 +32,8 @@
           console.log(e);
         }).then(function () {
           initColtura();
+        }).then(function () {
+          getColturaDefault();
         })
     }
 
@@ -38,6 +46,50 @@
         }
       }
     }
+
+    //Recupera i dati di default della coltura corrente
+    function getColturaDefault() {
+      colturaService.getColturaByName(vm.colturaCorrente.nome).then(function (result) {
+        vm.colturaDefault = result;
+      })
+    }
+
+
+    //Modifica lo stato e setta l'umidità di default
+    vm.setStato = function () {
+      for (var i = 0; i < vm.user.colture.length; i++) {
+        if (vm.user.colture[i].sensore == vm.vm.numSensore) {
+          vm.user.colture[i].statoCrescita = vm.modificaColtStato;
+
+          //Associa umidità di default per quello stato
+          switch (vm.modificaColtStato) {
+            case "Seme":
+              vm.user.colture[i].minUmidita = vm.colturaDefault.minUmidita[0];
+              vm.user.colture[i].maxUmidita = vm.colturaDefault.maxUmidita[0];
+              break;
+            case "Germoglio":
+              vm.user.colture[i].minUmidita = vm.colturaDefault.minUmidita[1];
+              vm.user.colture[i].maxUmidita = vm.colturaDefault.maxUmidita[1];
+              break;
+            case "PiantaAdulta":
+              vm.user.colture[i].minUmidita = vm.colturaDefault.minUmidita[2];
+              vm.user.colture[i].maxUmidita = vm.colturaDefault.maxUmidita[2];
+              break;
+            case "Raccolta":
+              vm.user.colture[i].minUmidita = vm.colturaDefault.minUmidita[3];
+              vm.user.colture[i].maxUmidita = vm.colturaDefault.maxUmidita[3];
+              break;
+          }
+        }
+        userService.updateColtureUtente(vm.user).then(function (response) {
+          if (response.data === "error") {
+            console.log("errore");
+          }
+        })
+        window.location.reload();
+      }
+    }
+
 
 
   }
